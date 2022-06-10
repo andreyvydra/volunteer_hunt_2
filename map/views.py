1d2c827aaa4a17488a588e7b3a6eeb26f1ec0483
andrey vydra <vidra8888@yandex.ru>
2022-06-10 21:30:02 +0300

    first version of maps

--- map/views.py
+++ map/views.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from hackaton_test.settings import MAPBOX_ACCESS_TOKEN
+from tasks.models import Task
+
+
+class MapView(View, LoginRequiredMixin):
+    template_name = "map/map.html"
+
+    def get(self, request):
+        context = dict()
+        context["MAPBOX_ACCESS_TOKEN"] = MAPBOX_ACCESS_TOKEN
+        context["tasks"] = Task.objects.values("point_on_map", "category", "name", "id").filter(creator_id=request.user.id)
+        for task in context["tasks"]:
+            task["lon"], task["lat"] = task["point_on_map"].split()
+        return render(request, self.template_name, context)
