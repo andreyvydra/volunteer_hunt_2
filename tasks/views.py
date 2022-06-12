bdd24b27d9f5152a2cd18634b0d893730de24bdd
andreyvydra <vidra8888@yandex.ru>
2022-06-12 14:10:54 +0300

    spelling mistake fix and jsquery fix

--- tasks/views.py
+++ tasks/views.py
@@ -30,14 +30,14 @@ class TaskView(View):
             'is_active_task': task.datetime >= timezone.now(),
             'belongs_to_user': task.creator.user_id == request.user.id,
         }
-
+        print(task.datetime)
+        print(timezone.now())
         volunteers_values_list = task.volunteers.values_list('user_id', flat=True).all()
         context['volunteers_number'] = len(volunteers_values_list)
         if request.user.id in volunteers_values_list:
             context['volunteer_on_task'] = True
         if len(volunteers_values_list) < task.max_volunteer:
             context['volunteer_not_enough'] = True
-
         context['MAPBOX_ACCESS_TOKEN'] = MAPBOX_ACCESS_TOKEN
         context['task_lon'], context['task_lat'] = task.point_on_map.split()
         context['task'].datetime = context['task'].datetime.astimezone().replace(tzinfo=None)
