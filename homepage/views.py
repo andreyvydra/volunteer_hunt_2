52fe0ca0b7a91e07bc581da3c971b413d9ae6586
andrey vydra <vidra8888@yandex.ru>
2022-06-12 14:18:07 +0300

    homepage info upd

--- homepage/views.py
+++ homepage/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.views import View
 
+from user.models import Volunteer, Employer
+
 
 class HomepageView(View):
     template_name = 'homepage/homepage.html'
 
     def get(self, request):
-        return render(request, HomepageView.template_name)
+        context = {}
+        context['count_volunteers'] = Volunteer.objects.values_list('id', flat=True).all().count()
+        context['count_employers'] = Employer.objects.values_list('id', flat=True).all().count()
+        return render(request, HomepageView.template_name, context=context)
