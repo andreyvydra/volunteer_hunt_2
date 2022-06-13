5c1d16c4b2830d1f703c18bbaf1edecc7a167aa9
andrey vydra <vidra8888@yandex.ru>
2022-06-13 08:14:50 +0300

    fix minor bug

--- homepage/views.py
+++ homepage/views.py
@@ -12,7 +12,15 @@ class HomepageView(View):
         context = dict()
         context['count_volunteers'] = Volunteer.objects.values_list('id', flat=True).all().count()
         context['count_employers'] = Employer.objects.values_list('id', flat=True).all().count()
-        context['most_popular_event'] = Task.objects.order_by('-volunteers').all()[0]
-        context['most_valuable_employer'] = Employer.objects.order_by('-my_tasks').all()[0]
+        context['most_popular_event'] = Task.objects.order_by('-volunteers').all()
+        if context['most_popular_event']:
+            context['most_popular_event'] = context['most_popular_event'][0]
+        else:
+            context['most_popular_event'] = None
+        context['most_valuable_employer'] = Employer.objects.order_by('-my_tasks').all()
+        if context['most_valuable_employer']:
+            context['most_valuable_employer'] = context['most_valuable_employer'][0]
+        else:
+            context['most_valuable_employer'] = None
         context['biggest_number_of_tasks_employer'] = len(context['most_valuable_employer'].my_tasks.all())
         return render(request, HomepageView.template_name, context=context)
